d00d1bc25a21179a97d3f8ac3cbf8397a3a261a4
Lubana-Akhter <lubana3093@gmail.com>
2023-08-14 19:45:32 +0600

    remove stack from error  middleware

--- hlr-backend/middleware/errorMiddleware.js
+++ hlr-backend/middleware/errorMiddleware.js
@@ -14,10 +14,10 @@ const errorHandler = (err, req, res, next) => {
     message = "Resource not found";
   }
 
-  res.status(statusCode).json({
-    message: message,
-    stack: process.env.NODE_ENV === "development" ? null : err.stack,
-  });
+  // res.status(statusCode).json({
+  //   message: message,
+  //   stack: process.env.NODE_ENV === "development" ? null : err.stack,
+  // });
 };
 
 module.exports = { notFound, errorHandler };
